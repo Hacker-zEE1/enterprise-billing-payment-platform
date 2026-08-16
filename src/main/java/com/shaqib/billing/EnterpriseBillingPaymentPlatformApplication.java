b909b7158471746e5a7beb128d2811685c16f572
Mohd Shaqib Anwar <msanwar.it2023@gmail.com>
2026-08-16 11:59:15 +0530

    feat: initialize Spring Boot application with PostgreSQL

---
+++ src/main/java/com/shaqib/billing/EnterpriseBillingPaymentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shaqib.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseBillingPaymentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseBillingPaymentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaqib/billing/EnterpriseBillingPaymentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaqib.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseBillingPaymentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
